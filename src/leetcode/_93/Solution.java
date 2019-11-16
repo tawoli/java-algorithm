84b359ee63386ae1b23d7bbcae9171f213cef3c8
Zhu Yupeng <tawoli@icloud.com>
2019-11-16 13:51:01 +0800

    93

---
+++ src/leetcode/_93/Solution.java
@@ -0,0 +1,31 @@
+package leetcode._93;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    private List<String> strings = new ArrayList<>();
+
+    public List<String> restoreIpAddresses(String s) {
+        findAddress(s, 0, 0, "");
+        return strings;
+    }
+
+    private void findAddress(String s, int index, int level, String res) {
+        if (level == 4) {
+            if (index == s.length()) {
+                strings.add(res.substring(0, res.length() - 1));
+            }
+            return;
+        }
+        for (int i = index; i < index + 3 && i < s.length(); i++) {
+            if (i == index && s.charAt(i) == '0') {
+                findAddress(s, i + 1, level + 1, res + s.charAt(i) + '.');
+                break;
+            }
+            if (Integer.parseInt(s.substring(index, i + 1)) <= 255) {
+                findAddress(s, i + 1, level + 1, res + s.substring(index, i + 1) + '.');
+            }
+        }
+    }
+}
