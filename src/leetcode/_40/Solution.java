2853421db50c6f19c5b6a99a5a9dbb930e895c27
Zhu Yupeng <tawoli@icloud.com>
2019-11-17 02:38:52 +0800

    40

---
+++ src/leetcode/_40/Solution.java
@@ -0,0 +1,33 @@
+package leetcode._40;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    private List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        combinationSum2(candidates, target, 0, new ArrayList<>());
+        return res;
+    }
+
+    private void combinationSum2(int[] candidates, int target, int index, List<Integer> c) {
+        if (target == 0) {
+            res.add(new ArrayList<>(c));
+            return;
+        }
+        for (int i = index; i < candidates.length; i++) {
+            if (candidates[i] > target) {
+                break;
+            }
+            if (i > index && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            c.add(candidates[i]);
+            combinationSum2(candidates, target - candidates[i], i + 1, c);
+            c.remove(c.size() - 1);
+        }
+    }
+}
