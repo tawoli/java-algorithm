d237aea11d34a6025bd14880a2063be166606218
Zhu Yupeng <tawoli@icloud.com>
2019-12-22 09:43:15 +0800

    203

---
+++ src/leetcode/_203/Solution.java
@@ -0,0 +1,32 @@
+package leetcode._203;
+
+public class Solution {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+
+        if (head == null) {
+            return null;
+        }
+        ListNode prev = head;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                prev.next = prev.next.next;
+            } else {
+                prev = prev.next;
+            }
+        }
+        return head;
+    }
+}
