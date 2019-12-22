8e5537b041c485ef1ee113532d72b3795c5ab256
Zhu Yupeng <tawoli@icloud.com>
2019-12-22 14:23:10 +0800

    804

---
+++ src/leetcode/_804/Solution.java
@@ -0,0 +1,19 @@
+package leetcode._804;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] codes = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        Set<String> set = new TreeSet<>();
+        for (String word : words) {
+            StringBuilder res = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
+                res.append(codes[word.charAt(i) - 'a']);
+            }
+            set.add(res.toString());
+        }
+        return set.size();
+    }
+}
